9d487b94f856342eac26d4952784e2fac2770dc9
zhoujustin <zhoujustin@gmail.com>
2017-10-27 22:42:02 -0700

    Disable GT9XX DEBUG

--- drivers/input/touchscreen/goodix/gt9xx.h
+++ drivers/input/touchscreen/goodix/gt9xx.h
@@ -73,9 +73,9 @@
 
 #define GTP_GESTURE_WAKEUP    0    // gesture wakeup
 
-#define GTP_DEBUG_ON          1
-#define GTP_DEBUG_ARRAY_ON    1
-#define GTP_DEBUG_FUNC_ON     1
+#define GTP_DEBUG_ON          0
+#define GTP_DEBUG_ARRAY_ON    0
+#define GTP_DEBUG_FUNC_ON     0
 
 #define PEN_DOWN 1
 #define PEN_RELEASE 0
